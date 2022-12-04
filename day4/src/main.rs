e742c51ba40111fa36e9bc6f8b904bca3535f181
Davide Cristini <davcri91@gmail.com>
2022-12-04 18:34:35 +0100

    day 4: part two

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,16 +1,14 @@
-use std::{collections::HashSet, fs, hash::Hash};
+use std::{cmp, fs};
 
 fn main() {
     let file_path = "./src/input.txt";
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
     let lines = contents.lines();
-    // let mut priorities_sum = 0;
 
     let mut fully_contained_assignments = 0;
-    for (idx, line) in lines.enumerate() {
-        println!("<--");
-        println!("{}", line);
+    let mut overlapping_assignments = 0;
 
+    for line in lines {
         let pair = line.split_once(",").unwrap();
         let first_elf_section_ids = pair.0;
         let second_elf_section_idx = pair.1;
@@ -22,11 +20,6 @@ fn main() {
         let second_elf_sections = second_elf_section_idx.split_once("-").unwrap();
         let second_elf_start: u32 = second_elf_sections.0.parse().unwrap();
         let second_elf_end: u32 = second_elf_sections.1.parse().unwrap();
-
-        println!(
-            "{} {} | {} {}",
-            first_elf_start, first_elf_end, second_elf_start, second_elf_end
-        );
 
         let first_elf_sections_count = first_elf_end - first_elf_start + 1;
         let second_elf_sections_count = second_elf_end - second_elf_start + 1;
@@ -41,10 +34,15 @@ fn main() {
             }
         }
 
-        println!("-->");
+        let bigger_start = cmp::max(first_elf_start, second_elf_start);
+        let smaller_end = cmp::min(first_elf_end, second_elf_end);
+        if bigger_start <= smaller_end {
+            overlapping_assignments += 1;
+        }
     }
     println!(
         "Fully contained assignments = {}",
         fully_contained_assignments
     );
+    println!("Overlapping assignments = {}", overlapping_assignments);
 }
