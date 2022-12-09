f918fdf905feb4a688c49bbc5051b838edae6b20
Davide Cristini <davcri91@gmail.com>
2022-12-10 00:26:53 +0100

    day 9: part one

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -2,14 +2,90 @@ use core::fmt;
 use std::str::FromStr;
 
 fn main() {
-    let input_file = include_str!("sample.txt");
+    let input_file = include_str!("input.txt");
     let steps = parse_input(input_file);
-
-    for s in &steps {
-        println!("{s}");
-    }
+    let walked_cells = solve_part_1(steps);
+    println!("{} ", walked_cells);
 }
 
+fn solve_part_1(steps: Vec<Step>) -> usize {
+    let mut coords_tail: Vec<Coord> = Vec::new();
+    let mut head_coord = Coord { x: 0, y: 0 }; // current coord
+    let mut tail_coord = Coord { x: 0, y: 0 }; // current coord
+
+    // record starting position
+    coords_tail.push(Coord {
+        x: head_coord.x,
+        y: head_coord.y,
+    });
+
+    for s in steps {
+        // println!("---{s}");
+        for _ in 0..s.count {
+            // move head
+            let mut new_head_coord = Coord {
+                x: head_coord.x,
+                y: head_coord.y,
+            };
+            match s.dir {
+                Dir::Left => {
+                    new_head_coord.x -= 1;
+                }
+                Dir::Right => {
+                    new_head_coord.x += 1;
+                }
+                Dir::Up => {
+                    new_head_coord.y -= 1;
+                }
+                Dir::Down => {
+                    new_head_coord.y += 1;
+                }
+            }
+            head_coord.x = new_head_coord.x;
+            head_coord.y = new_head_coord.y;
+
+            let diff = Coord {
+                x: head_coord.x - tail_coord.x,
+                y: head_coord.y - tail_coord.y,
+            };
+            // If the head is ever two steps directly up, down, left, or right
+            // from the tail, the tail must also move one step in that direction
+            // so it remains close enough:
+            if diff.y == 0 {
+                if diff.x.abs() == 2 {
+                    tail_coord.x += diff.x.signum();
+                }
+            } else if diff.x == 0 {
+                if diff.y.abs() == 2 {
+                    tail_coord.y += diff.y.signum();
+                }
+            }
+            // Otherwise, if the head and tail aren't touching and aren't in the
+            // same row or column, the tail always moves one step diagonally to
+            // keep up:
+            if diff.x.abs() + diff.y.abs() > 2
+                && tail_coord.x != head_coord.x
+                && tail_coord.y != head_coord.y
+            {
+                tail_coord.x += diff.x.signum();
+                tail_coord.y += diff.y.signum();
+            }
+
+            // println!("h{} t{}", head_coord, tail_coord);
+
+            if !coords_tail.contains(&tail_coord) {
+                coords_tail.push(Coord {
+                    x: tail_coord.x,
+                    y: tail_coord.y,
+                });
+            }
+        }
+    }
+
+    return coords_tail.len();
+}
+
+#[derive(PartialEq)]
 enum Dir {
     Left,
     Right,
@@ -17,6 +93,17 @@ enum Dir {
     Down,
 }
 
+#[derive(PartialEq)]
+struct Coord {
+    x: i32,
+    y: i32,
+}
+
+impl fmt::Display for Coord {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "({}, {})", self.x, self.y)
+    }
+}
 impl fmt::Display for Dir {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let mut str_repr = "";
